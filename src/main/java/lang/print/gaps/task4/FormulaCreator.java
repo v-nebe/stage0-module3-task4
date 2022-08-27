a266921400ce29ed51e79dcaf67692ecab6c24a4
v-nebe <katya.shauneva@gmail.com>
2022-08-27 10:59:42 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+
+        System.out.println("(9" + a + "^2 - 5" + b + " + 2 + " + a + " - 7)"
+                + "* ((" + a + " + " + b + " - 4" + a + b + ")/ 2");
     }
 }
